b37672a46dffa3bed7d752459c5302d50a8455c7
StephenLunn112 <stephenlunn84@gmail.com>
2026-02-19 01:46:04 +0000

    Addressables installed

---
+++ Brackeys Game Jam 2026.1/Assets/WwiseData/Bank/Wwise_IDs.h
@@ -0,0 +1,110 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CRICKET_JUMP = 515483981U;
+        static const AkUniqueID CRICKET_LAND = 3381550706U;
+        static const AkUniqueID FLOORVINE_EATING = 2689584014U;
+        static const AkUniqueID PLAY_MOSQUITO = 1534211419U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
+        static const AkUniqueID PLAYER_LAND = 3629196698U;
+        static const AkUniqueID PLAYER_MOVEMENT = 541470702U;
+        static const AkUniqueID UMBRELLA_CLOSE = 3217770052U;
+        static const AkUniqueID UMBRELLA_OPEN = 89220416U;
+        static const AkUniqueID VINE_CATCH = 1532741045U;
+        static const AkUniqueID VINE_EATING = 2783995914U;
+        static const AkUniqueID VINE_HIT = 3793263743U;
+        static const AkUniqueID VINE_LASTHIT = 2514947935U;
+        static const AkUniqueID VINE_STOPRECOVERING = 239772940U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS_SURFACE
+        {
+            static const AkUniqueID GROUP = 1162743606U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GLASS = 2449969375U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS_SURFACE
+
+        namespace MOVEMENT_TYPE
+        {
+            static const AkUniqueID GROUP = 1088160865U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace MOVEMENT_TYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID MOSQUITO_SWARM = 3385394437U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID PULL_TENSION = 2763939063U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID DIAL = 2447300951U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
