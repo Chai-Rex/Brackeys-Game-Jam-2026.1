2eb259e72e469dc74b9b73ccbb5b182baf15b1fa
ViriusLevan <Mantor562@gmail.com>
2026-02-20 12:33:15 +0700

    Skill Tree 01
    Refactored the Skill Tree Nodes to have slightly more robust editor functionality, not yet fully tested with gameplay

---
+++ Brackeys Game Jam 2026.1/Assets/_Project/_Scripts/Player/SkillTree/SkillNodeConnector.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class SkillNodeConnector : MonoBehaviour
+{
+    public RectTransform target;
+    public RectTransform from;
+
+    private RectTransform rect;
+    public float thickness = 10f;
+
+    public SkillTreeNode fromSTN, toSTN;
+
+    private void OnEnable()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (!target) return;
+
+        Vector2 direction = target.position - rect.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        rect.rotation = Quaternion.Euler(0, 0, angle);
+        
+        // Midpoint position
+        rect.position = (from.position + target.position) / 2f;
+        
+        
+        Vector3 posA = from.localPosition;
+        Vector3 posB = target.localPosition;
+        
+        
+        // Direction
+        Vector3 dir = (posB - posA);
+        
+        //Debug.Log($"{posB}-{posA}={dir}");
+        
+        // Length
+        float length = dir.magnitude;
+        
+        rect.sizeDelta = new Vector2(length, thickness);
+    }
+
+    public void DestroyReferences()
+    {
+        fromSTN.nextNodes.Remove(toSTN);
+        fromSTN.connectedConnectors.Remove(this);
+
+        toSTN.precedingNodes.Remove(fromSTN);
+        toSTN.connectedConnectors.Remove(this);
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.delayCall += () =>
+        {
+            if (this)
+                DestroyImmediate(gameObject);
+        };
+#endif
+        
+    }
+}
---
+++ Brackeys Game Jam 2026.1/Assets/_Project/_Scripts/Player/SkillTree/SkillTreeNodeOnDestroy.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SkillTreeNodeOnDestroy : MonoBehaviour
+{
+
+    public SkillTreeNode parent;
+
+    private void OnValidate()
+    {
+        parent = GetComponent<SkillTreeNode>();
+    }
+
+
+    private void OnDestroy()
+    {
+        for (int i = parent.connectedConnectors.Count-1; i>-1 ; i--)
+        {
+            if (parent.connectedConnectors[i])
+            {
+                parent.connectedConnectors[i].DestroyReferences();
+            }
+        }
+    }
+}
