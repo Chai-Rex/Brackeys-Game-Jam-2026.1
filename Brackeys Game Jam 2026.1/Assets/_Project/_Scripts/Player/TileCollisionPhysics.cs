62b164b9809227936b2b10c59e464f963da208a3
Lily Peterson <lilypeterson444@gmail.com>
2026-02-22 02:57:41 -0500

    Gameplay Map

--- Brackeys Game Jam 2026.1/Assets/_Project/_Scripts/Player/TileCollisionPhysics.cs
+++ Brackeys Game Jam 2026.1/Assets/_Project/_Scripts/Player/TileCollisionPhysics.cs
@@ -7,20 +7,21 @@ public class TileCollisionPhysics : MonoBehaviour
     [SerializeField] private TileDatabase tileDatabase;
     [SerializeField] private Health playerHealth;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collision2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Vector3Int cellPosition = tilemap.WorldToCell(other.transform.position);
+            Debug.Log($"Collided with tile at Position: {other.transform.position}, Cell Position: {cellPosition}");
             TileData tileData = tileDatabase.tileDatabase[tilemap.GetTile(cellPosition).name];
             Debug.Log($"Collided with tile: {tileData.tileName}");
             if (tileData != null && tileData.physicalMaterial != null)
             {
                 other.GetComponent<Collider2D>().sharedMaterial = tileData.physicalMaterial;
                 Debug.Log($"Applied physical material: {tileData.physicalMaterial.name}");
-                if(tileData.name == "Lava")
+                if(tileData.name == "Lava Rule Tiles")
                 {
-                    playerHealth.Drain(4f * Time.deltaTime); // Adjust the damage rate as needed
+                    playerHealth.Drain(4f); // Adjust the damage rate as needed
                 }
             }
             if(tileData.physicalMaterial == null)
